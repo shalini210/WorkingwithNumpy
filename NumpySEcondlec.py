6c16e6e9f492e7ed8ad902096959e0b18d8e77de
shalini210 <84281006+shalini210@users.noreply.github.com>
2022-11-09 06:59:00 +0530

    second class numpy

--- NumpySEcondlec.py
+++ NumpySEcondlec.py
@@ -7,3 +7,12 @@ print(a2)
 print("Dimension of array a is : "+ str(a.ndim))
 print("Dimension of array 2 is : "+ str(a2.ndim))
 print("Each element of array a has size : ", a.itemsize, " bytes")
+print("value at index 1,2 is ", a2[1,2])
+print("max element in array a is ",a.max())
+print("max element in array a2 is ",a2.max())
+print("min element in array a is ", a.min());
+print("min element in array a2 is ", a2.min())
+print(a2+1)
+print(a2)
+a2= a2+1
+print(a2)
