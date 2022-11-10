9062a33bc533cb905ec700d0de954c293ecb2892
shalini210 <84281006+shalini210@users.noreply.github.com>
2022-11-10 07:38:23 +0530

    third class numpy

--- WorkingqithTwo2Darrays.py
+++ WorkingqithTwo2Darrays.py
@@ -1,6 +1,12 @@
 import numpy
+zerod = numpy.array(90);
+oned = numpy.array([32,43,12,32])
+print("dimension if array zerod is ", zerod.ndim)
+print("dimension if array oned is ", oned.ndim)
 a = numpy.array([[12,2,4],[5,3,1],[34,55,77]])
-
+threed = numpy.array([[[12,2,4],[5,3,1],[34,55,77]],[[12,2,4],[5,3,1],[34,55,77]]])
+print("dimension if array a is ", a.ndim)
+print("dimension if array threed is ", threed.ndim)
 b = numpy.array([[2,1,4],[4,6,7],[34,5,6]])
 print(a)
 print(b)
